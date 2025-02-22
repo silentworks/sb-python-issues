42a7dfecd186a262ed51c3d3270c65a4f888602b
Andrew Smith <a.smith@silentworks.co.uk>
2025-02-22 13:14:07 +0000

    Update with get_public_url method

--- supabase_discord_pdf_uploading/app.py
+++ supabase_discord_pdf_uploading/app.py
@@ -12,15 +12,18 @@ key = os.environ.get("SUPABASE_KEY", "")
 supabase: Client = create_client(url, key)
 
 try:
+    file_path = "upload/sample_explain.pdf"
     # Read file content
     with open("./c4611_sample_explain.pdf", "rb") as file:
         # Upload to Supabase storage
         result = supabase.storage.from_('user_pdfs').upload(
-            path="sample_explain.pdf",
+            path=file_path,
             file=file,
             file_options={"content-type": "application/pdf"}
         )
 
-    print(f"RESPONSE: {result}")
+    response = supabase.storage.from_('user_pdfs').get_public_url(file_path)
+
+    print(f"RESPONSE: {response}")
 except APIError as e:
     print(f"ERROR: {e}")
