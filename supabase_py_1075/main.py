bc0ce2b6115cef84063101db5c9836e900f459ed
Andrew Smith <a.smith@silentworks.co.uk>
2025-04-26 10:23:07 +0000

    Add fly.io setup for testing with plain httpx

--- supabase_py_1075/main.py
+++ supabase_py_1075/main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI, Depends
-from supabase import AsyncClient as Client, acreate_client
+from httpx import AsyncClient, AsyncHTTPTransport, Limits, Timeout
+from supabase import AsyncClient as Client, AsyncClientOptions, acreate_client
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,7 +15,8 @@ app = FastAPI()
 # Dependency that creates a new Supabase client per request
 async def get_supabase_client():
     supabase = await acreate_client(
-        SUPABASE_URL, SUPABASE_ANON_KEY
+        SUPABASE_URL, SUPABASE_ANON_KEY,
+        options=AsyncClientOptions(postgrest_client_timeout=Timeout(None))
     )  # Creates a new client per request
     return supabase
 
@@ -24,3 +26,31 @@ async def test_supabase(supabase: Client = Depends(get_supabase_client)):
     """A route that uses Supabase."""
     res = await supabase.table("cities").select("*").limit(1).execute()
     return res
+
+@app.get("/httpx")
+async def test_httpx():
+    """A route that uses Supabase."""
+    transport = AsyncHTTPTransport(
+        retries=3,
+        http2=True,
+        limits=Limits(
+            max_connections=100,
+            max_keepalive_connections=1,
+            keepalive_expiry=None,
+        )
+    )
+    headers = {"Authorization": f"Bearer {SUPABASE_ANON_KEY}", "apiKey": SUPABASE_ANON_KEY, "Connection": "close"}
+    async with AsyncClient(
+        transport=transport,
+        headers=headers,
+        base_url=f"{SUPABASE_URL}/rest/v1",
+        http2=True
+    ) as client:
+        res = await client.request(
+            "GET",
+            "/cities",
+            params={"select": "*", "limit": 1},
+            timeout=None
+        )
+
+    return res.json()
